b4a79b54679d4e19e9d5218492340308626414b6
MikhailShcherbakov90 <scherbakov90@gmail.com>
2021-05-30 17:06:09 +0300

    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ app/src/main/java/com/example/lesson1/Person.java
@@ -0,0 +1,16 @@
+package com.example.lesson1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Person extends AppCompatActivity{
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.person);
+        }
+
+}
